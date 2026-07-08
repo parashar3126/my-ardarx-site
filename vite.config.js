8f6d5225e4e388e7c5af0a6c7e35cb54f8b5d7c2
Amit Parashar <parasharamit440@gmail.com>
2026-07-08 23:52:29 +0530

    Simplify plugin array in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: './', // यह लाइन क्लाउड बिल्ड को सही पाथ देती है
 })
